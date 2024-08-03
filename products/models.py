e32cc236f99962c704b04777687dd25f76093459
akhilmohan123 <akhilmohan8547@gmail.com>
2024-08-03 08:44:46 +0530

    intial commit

---
+++ products/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    LIVE=1
+    DELETE=0
+    DELETE_CHOICES=((LIVE,"Live"),(DELETE,"Delete"))
+    title=models.CharField(max_length=200)
+    price=models.FloatField()
+    description=models.TextField()
+    image=models.ImageField(upload_to="media")
+    priority=models.IntegerField(default=0)
+    delete=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
+    created_At=models.DateTimeField(auto_now_add=True)
+    updated_At=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
